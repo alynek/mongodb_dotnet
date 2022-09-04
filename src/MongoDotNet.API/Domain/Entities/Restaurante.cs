3a549690e96f4275d86b75ce8b5a99316de9ba55
alynek <alyne0x67@gmail.com>
2022-09-04 19:05:16 -0300

    Create domain classes

---
+++ src/MongoDotNet.API/Domain/Entities/Restaurante.cs
@@ -0,0 +1,21 @@
+﻿using MongoDotNet.API.Domain.Enums;
+using MongoDotNet.API.Domain.ValueObjects;
+
+namespace MongoDotNet.API.Domain.Entities
+{
+    public class Restaurante
+    {
+        public string Id { get; private set; }
+        public string Nome { get; private set; }
+        public ECozinha Cozinha { get; private set; }
+
+        public Endereco Endereco { get; private set; }
+
+        public Restaurante(string id, string nome, ECozinha cozinha)
+        {
+            Id = id;
+            Nome = nome;
+            Cozinha = cozinha;
+        }
+    }
+}
---
+++ src/MongoDotNet.API/Domain/ValueObjects/Endereco.cs
@@ -0,0 +1,22 @@
+﻿namespace MongoDotNet.API.Domain.ValueObjects
+{
+    public class Endereco
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Cidade { get; private set; }
+        public string UF { get; private set; }
+        public string Cep { get; private set; }
+
+        public Endereco(){}
+
+        public Endereco(string logradouro, string numero, string cidade, string uf, string cep)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Cidade = cidade;
+            UF = uf;
+            Cep = cep;
+        }
+    }
+}
