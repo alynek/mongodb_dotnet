8aa2e376a9bb3bf92e1c18cd6c79d2665f1b7562
alynek <alyne0x67@gmail.com>
2022-09-07 21:59:28 -0300

    Create RestauranteController

---
+++ src/MongoDotNet.API/Controllers/RestauranteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDotNet.API.Domain.Enums;
+using MongoDotNet.API.Dtos;
+
+namespace MongoDotNet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestauranteController : ControllerBase
+    {
+        [HttpPost("novo-restaurante")]
+        public ActionResult AdicionarRestaurante([FromBody] NovoRestauranteDto novoRestauranteDto)
+        {
+            var tipoComida = TipoDeComidaHelper.ConverterDeInteiro(novoRestauranteDto.TipoDeComida);
+
+            var restaurante = novoRestauranteDto.NovoRestaurante(tipoComida);
+            var endereco = novoRestauranteDto.NovoEndereco();
+
+            restaurante.AtribuirEndereco(endereco);
+
+            if (!restaurante.Validar())
+            {
+                return BadRequest(new { errors = restaurante.ValidationResult.Errors.Select(e => e.ErrorMessage) });
+            }
+
+            _restauranteRepository.Inserir(restaurante);
+            return Ok(new { data = "Restaurante inserido com sucesso" });
+        }
+    }
+}
