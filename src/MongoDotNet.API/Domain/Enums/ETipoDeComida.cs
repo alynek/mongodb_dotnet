b84f76330fbce36a6c1002d0df0249e690fdffd1
alynek <alyne0x67@gmail.com>
2022-09-07 21:58:25 -0300

    Update enum name

---
+++ src/MongoDotNet.API/Domain/Enums/ETipoDeComida.cs
@@ -0,0 +1,21 @@
+﻿namespace MongoDotNet.API.Domain.Enums
+{
+    public enum ETipoDeComida
+    {
+        Brasileira = 1,
+        Italiana = 2,
+        Arabe = 3,
+        Japonesa = 4,
+        FastFood = 5
+    }
+
+    public static class TipoDeComidaHelper
+    {
+        public static ETipoDeComida ConverterDeInteiro(int valor)
+        {
+            if (Enum.TryParse(valor.ToString(), out ETipoDeComida tipoDeComida)) return tipoDeComida;
+
+            throw new ArgumentOutOfRangeException("Não foi possível converter de int para enum");
+        }
+    }
+}
