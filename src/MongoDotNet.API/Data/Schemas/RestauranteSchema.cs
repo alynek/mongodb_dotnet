aef6b50d84178eb0a28afa2667af832cbfa0f1de
alynek <alyne0x67@gmail.com>
2022-09-11 17:23:13 -0300

    Add BsonType annotation

--- src/MongoDotNet.API/Data/Schemas/RestauranteSchema.cs
+++ src/MongoDotNet.API/Data/Schemas/RestauranteSchema.cs
@@ -1,9 +1,12 @@
-﻿using MongoDotNet.API.Domain.Enums;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDotNet.API.Domain.Enums;
 
 namespace MongoDotNet.API.Data.Schemas
 {
     public class RestauranteSchema
     {
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Nome { get; set; }
         public ETipoDeComida TipoDeComida { get; set; }
