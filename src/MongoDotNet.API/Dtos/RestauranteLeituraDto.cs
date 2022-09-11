7463a661bd2b8dd19ce446457c894a06e077c670
alynek <alyne0x67@gmail.com>
2022-09-11 17:24:44 -0300

    Create RestauranteLeituraDto

---
+++ src/MongoDotNet.API/Dtos/RestauranteLeituraDto.cs
@@ -0,0 +1,12 @@
+﻿using MongoDotNet.API.Domain.Enums;
+
+namespace MongoDotNet.API.Dtos
+{
+    public class RestauranteLeituraDto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string TipoComida { get; set; }
+        public string Cidade { get; set; }
+    }
+}
