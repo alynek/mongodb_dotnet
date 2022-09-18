a75949fbec5dbda4b4863e7dcfbe2ac809bb16a6
alynek <alyne0x67@gmail.com>
2022-09-18 16:28:09 -0300

    Create RestauranteTop3Dto

---
+++ src/MongoDotNet.API/Dtos/RestauranteTop3Dto.cs
@@ -0,0 +1,11 @@
+﻿namespace MongoDotNet.API.Dtos
+{
+    public class RestauranteTop3Dto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int Cozinha { get; set; }
+        public string Cidade { get; set; }
+        public double Estrelas { get; set; }
+    }
+}
