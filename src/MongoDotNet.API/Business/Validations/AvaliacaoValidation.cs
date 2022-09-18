28d981639c427b7b1511d7f534484224d258f4bc
alynek <alyne0x67@gmail.com>
2022-09-18 16:24:27 -0300

    Create AvaliacaoValidation class

---
+++ src/MongoDotNet.API/Business/Validations/AvaliacaoValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MongoDotNet.API.Domain.ValueObjects;
+
+namespace MongoDotNet.API.Business.Validations
+{
+    public class AvaliacaoValidation : AbstractValidator<Avaliacao>
+    {
+        public AvaliacaoValidation()
+        {
+            RuleFor(a => a.Estrelas)
+                .GreaterThan(0).WithMessage("Numero de estrelas deve ser maior que zero")
+                .LessThanOrEqualTo(5).WithMessage("Número de estrelas deve ser menor ou igual a cinco");
+
+            RuleFor(a => a.Comentario)
+                .NotEmpty().WithMessage("Comentário não pode ser vazio")
+                .MaximumLength(100).WithMessage("Comentário pode ter no máximo 100 caracteres");
+        }
+    }
+}
