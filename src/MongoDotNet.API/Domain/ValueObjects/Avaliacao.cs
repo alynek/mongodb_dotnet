5474a3a2ae8c16dd9b46cf73312c59e7dbbf6ef9
alynek <alyne0x67@gmail.com>
2022-09-18 16:24:11 -0300

    Create value object Avaliacao

---
+++ src/MongoDotNet.API/Domain/ValueObjects/Avaliacao.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+using MongoDotNet.API.Business.Validations;
+
+namespace MongoDotNet.API.Domain.ValueObjects
+{
+    public class Avaliacao
+    {
+        public int Estrelas { get; set; }
+        public string Comentario { get; set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public Avaliacao() { }
+        public Avaliacao(int estrelas, string comentario)
+        {
+            Estrelas = estrelas;
+            Comentario = comentario;
+        }
+
+        public bool Validar()
+        {
+            ValidationResult = new AvaliacaoValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
